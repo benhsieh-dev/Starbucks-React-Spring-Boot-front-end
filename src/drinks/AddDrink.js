41291111e0e745b017f0bdddc878bcec2543c1aa
Ben Hsieh <bqh5026@gmail.com>
2023-04-25 13:39:46 -0500

    Add post menu item functionality from React to Spring Boot MySQL database

--- src/drinks/AddDrink.js
+++ src/drinks/AddDrink.js
@@ -1,68 +1,109 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AddDrink() {
+
+    let navigate = useNavigate(); 
+
+  const [drink, setDrink] = useState({
+    name: "",
+    category: "",
+    subcategory: "",
+    price: "",
+    imageUrl: "",
+  });
+
+  const { name, category, subcategory, price, imageUrl } = drink;
+
+  const onInputChange = (e) => {
+    setDrink({ ...drink, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/drink", drink)
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Add Menu Item</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter Menu Item Name"
-              name="name"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Category" className="form-label">
-              Category
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter Menu Item Category"
-              name="category"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Subcategory" className="form-label">
-              Subcategory
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter Menu Item Subcategory"
-              name="subcategory"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Price" className="form-label">
-              Price
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter Menu Item Price"
-              name="price"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="ImageUrl" className="form-label">
-              Image Url
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter Menu Item Image URL"
-              name="imageUrl"
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">Submit</button>
-          <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+          <form onSubmit={(e)=>onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Menu Item Name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Category" className="form-label">
+                Category
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Menu Item Category"
+                name="category"
+                value={category}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Subcategory" className="form-label">
+                Subcategory
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Menu Item Subcategory"
+                name="subcategory"
+                value={subcategory}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Price" className="form-label">
+                Price
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Menu Item Price"
+                name="price"
+                value={price}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="ImageUrl" className="form-label">
+                Image Url
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Menu Item Image URL"
+                name="imageUrl"
+                value={imageUrl}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger mx-2">
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </div>
