ec70927328c37a88aa6a965f18d95105f5c2f9c1
Ben Hsieh <bqh5026@gmail.com>
2023-04-25 12:45:03 -0500

    Add input fields and submit button

--- src/drinks/AddDrink.js
+++ src/drinks/AddDrink.js
@@ -1,17 +1,69 @@
-import React from 'react'
+import React from "react";
 
 export default function AddDrink() {
   return (
-    <div className='container'>
-        <div className='row'>
-            <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
-                <h2 className='text-center m-4'>Add Menu Item</h2>
-                <div className='mb-3'>
-                    <label htmlFor='Name' className='form-label'>Name</label>
-                    <input type={'text'} className='form-control' placeholder='Enter Menu Item Name' name='name' />
-                </div>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Add Menu Item</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter Menu Item Name"
+              name="name"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Category" className="form-label">
+              Category
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter Menu Item Category"
+              name="category"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Subcategory" className="form-label">
+              Subcategory
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter Menu Item Subcategory"
+              name="subcategory"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Price" className="form-label">
+              Price
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter Menu Item Price"
+              name="price"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="ImageUrl" className="form-label">
+              Image Url
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter Menu Item Image URL"
+              name="imageUrl"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">Submit</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
