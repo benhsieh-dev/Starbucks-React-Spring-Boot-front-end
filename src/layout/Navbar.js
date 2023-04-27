3ddc203f4030ab7e7af58bc71fa4e3eaa2c186ed
Ben Hsieh <bqh5026@gmail.com>
2023-04-26 22:26:41 -0500

    Redirect to homepage when clicking on logo

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,12 +5,14 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <img
-          src={require("../assets/images/starbucks-logo.png")}
-          alt="Starbucks logo"
-          width="60"
-          height="60"
-        ></img>
+        <Link to="/">
+          <img
+            src={require("../assets/images/starbucks-logo.png")}
+            alt="Starbucks logo"
+            width="60"
+            height="60"
+          ></img>
+        </Link>
         <div className="container-fluid">
           <div className="left-navigation-menu">
             <a className="navbar-brand" href="www.google.com">
