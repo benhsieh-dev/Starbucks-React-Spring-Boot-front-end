8218234b7bfc34ae2703b90e4d64b41af9d4b235
Ben Hsieh <bqh5026@gmail.com>
2023-04-29 22:21:47 -0500

    Add login user functionality to React.js

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,85 @@
-import React from "react"; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function SignIn() {
-    return (
-        <div>
-            <h2>Sign In page under construction</h2>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login(event) {
+    event.preventDefault();
+    try {
+      await axios
+        .post("http://localhost:8080/api/v1/customer/login", {
+          email: email,
+          password: password,
+        })
+        .then(
+          (res) => {
+            console.log(res.data);
+            if (res.data.message === "Email does not exist") {
+              alert("Email does not exist");
+            } else if (res.data.message === "Login Successful!") {
+              navigate("/menu");
+            } else {
+              alert("Email and password are incorrect");
+            }
+          },
+          (fail) => {
+            console.log(fail);
+          }
+        );
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <h2>Login</h2>
+          <hr />
         </div>
-    )
+
+        <div className="row">
+          <div className="col-sm-6">
+            <form>
+              <div className="form-group">
+                <label>email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <label>password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary" onClick={login}>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
