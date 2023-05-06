128a2984d859f9ef21934f91e3a4c4c439956e34
Ben Hsieh <bqh5026@gmail.com>
2023-05-05 21:11:33 -0500

    Update products component and css

--- src/pages/Menu/AllProducts/AllProducts.js
+++ src/pages/Menu/AllProducts/AllProducts.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function AllProducts() {
   return (
     <div>
-      <h2>Hot Teas</h2>
-      <h3>Chai Teas</h3>
+      <h2>Menu</h2>
+      <h3>Drinks</h3>
+      <div className="image-container">
+        <img
+          className="menu-item left"
+          alt="Oleato™"
+          src="https://globalassets.starbucks.com/assets/3DB27F5E792C48E9B867A443AA648D26.jpg?impolicy=1by1_wide_topcrop_630"
+        ></img>
+        <span className="right">Oleato™</span>
+      </div>
     </div>
-  )
+  );
 }
