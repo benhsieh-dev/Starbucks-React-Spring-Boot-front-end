05cae9c2ea17b03b5069fb5ba38164fd346c8477
Ben Hsieh <bqh5026@gmail.com>
2024-01-23 23:50:30 -0600

    Update account page

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import "./Account.css";
 
 export default function Account() {
   return (
-    <div>
-      <img
-        src={require("../../assets/images/137-88178.jpg")}
-        alt="Starbucks Deals"
-      ></img>
+    <div className="account-page-top">
+      <div className="account-page-left">
+        <img className="account-image"
+          src={require("../../assets/images/137-88178.jpg")}
+          alt="Starbucks Deals"
+        ></img>
+      </div>
+      <div className="account-page-right">
+        <p>New Year, New Yays</p>
+        <br />
+        <p>Jump-start your year with a week full of offers in the Starbucks app.*</p>
+      </div>
     </div>
   );
 }
