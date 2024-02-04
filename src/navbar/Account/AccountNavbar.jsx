c01be4549af252207de15235cdfba6bf7eed161b
Ben Hsieh <bqh5026@gmail.com>
2024-02-03 21:47:34 -0600

    Update splash and account page

--- src/navbar/Account/AccountNavbar.jsx
+++ src/navbar/Account/AccountNavbar.jsx
@@ -9,34 +9,25 @@ export default function AccountNavbar() {
     <div>
       <nav>
         <div className="acct-navbar-container">
-
           <div className="left-navigation-menu">
-              <Link to="/">
-                <img
-                  src={require("../../assets/images/starbucks-logo.png")}
-                  alt="Starbucks logo"
-                  width="60"
-                  height="60"
-                ></img>
-              </Link>
-              <Link to="/menu">
-                MENU
-              </Link>
-              <Link to="/rewards">
-                REWARDS
-              </Link>
-              <Link to="/giftcards">
-                GIFT CARDS
-              </Link>
-              <Link to="/adddrink">ADD MENU ITEM</Link>
-            </div>
+            <Link to="/">
+              <img
+                src={require("../../assets/images/starbucks-logo.png")}
+                alt="Starbucks logo"
+                width="60"
+                height="60"
+              ></img>
+            </Link>
+            <Link to="/menu">MENU</Link>
+            <Link to="/rewards">REWARDS</Link>
+            <Link to="/giftcards">GIFT CARDS</Link>
+            <Link to="/adddrink">ADD MENU ITEM</Link>
+          </div>
 
           <div className="right-navigation-menu">
             <div className="icon">
               <FontAwesomeIcon className="fa" icon={faLocationDot} />
-              <Link to="/storelocator">
-                Find a store
-              </Link>
+              <Link to="/storelocator">Find a store</Link>
             </div>
             <div className="dropdown">
               <div className="navbar-account icon">
@@ -44,15 +35,33 @@ export default function AccountNavbar() {
                 <button class="button">Account</button>
               </div>
               <div className="content">
-                <a href="www.google.com">Card Management</a>
-                <a href="www.google.com">My Rewards</a>
-                <a href="www.google.com">History</a>
-                <a href="www.google.com">Personal Info</a>
-                <a href="www.google.com">Payment Method</a>
-                <a href="www.google.com">Privacy and data</a>
-                <a href="www.google.com">Settings</a>
-                <hr />
-                <a href="www.google.com">Sign Out</a>
+                <ul>
+                  <li>
+                    <a href="www.google.com">Card Management</a>
+                  </li>
+                  <li>
+                    <a href="www.google.com">My Rewards</a>
+                  </li>
+                  <li>
+                    <a href="www.google.com">History</a>
+                  </li>
+                  <li>
+                   <a href="www.google.com">Personal Info</a>
+                  </li>
+                  <li>
+                    <a href="www.google.com">Payment Method</a>
+                  </li>
+                  <li>
+                   <a href="www.google.com">Privacy and data</a>
+                  </li>
+                  <li>
+                   <a href="www.google.com">Settings</a>
+                  </li>
+                  <hr />
+                  <li>
+                    <a href="www.google.com">Sign Out</a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
