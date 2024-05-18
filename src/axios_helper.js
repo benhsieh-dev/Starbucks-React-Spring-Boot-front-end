3951143213015ebb24fee9fae00391e561f5e774
Ben Hsieh <bqh5026@gmail.com>
2024-05-17 22:51:34 -0500

    Edit axios helper

--- src/axios_helper.js
+++ src/axios_helper.js
@@ -3,6 +3,10 @@ import axios from "axios";
 axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.post["Content-Type"] = 'application/json';
 
+export const getAuthToken = () => {
+    return window.localStorage.getItem("auth_token");
+};
+
 export const request = (method, url, data) => {
     return axios({
         method: method,
