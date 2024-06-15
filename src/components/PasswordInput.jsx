f3f3bac9ee0f5636fc6a10c07f5fc45e197adbaf
Ben Hsieh <bqh5026@gmail.com>
2024-06-14 23:47:55 -0500

    Update password input file

---
+++ src/components/PasswordInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export const PasswordInput = (props) => {
+  return (
+    <div>PasswordInput</div>
+  )
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PasswordInput)
