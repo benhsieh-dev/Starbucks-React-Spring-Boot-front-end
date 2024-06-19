006675b0a21a31b1eeedb65c5da9a3cda57d1638
Ben Hsieh <bqh5026@gmail.com>
2024-06-18 22:41:23 -0500

    Update password input component

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PasswordInput() {
+    const [password, setPassword] = useState("");
+    
   return (
     <div>
         <form>
