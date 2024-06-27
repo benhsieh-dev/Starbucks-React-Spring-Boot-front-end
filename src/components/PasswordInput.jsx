8a734448cb1c6dacb03c13f5c101e35ad6e95cbe
Ben Hsieh <bqh5026@gmail.com>
2024-06-26 22:11:37 -0500

    Install tailwind css

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -6,20 +6,21 @@ export default function PasswordInput() {
     const [visible, setVisible] = useState(false); 
 
   return (
-    <div>       
-        <label
-            htmlFor='password'>Password</label>
-        <div>
-            <input
-                value={"password"}
-                type={visible ? "text" : "password"}
-                id="password"
-                placeholder='password'
-                onChange={e => setPassword(e.target.value)}
-            >
-            </input>            
+    <div>
+      <label htmlFor="password">Password</label>
+      <div>
+        <input
+          value={"password"}
+          type={visible ? "text" : "password"}
+          id="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <div className="p-2">
+          {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
