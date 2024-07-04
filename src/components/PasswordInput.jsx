d3d07f8c353fddc229dc2d1aca123658db59adf9
Ben Hsieh <bqh5026@gmail.com>
2024-07-03 21:49:23 -0500

    Update password input component

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -7,16 +7,21 @@ export default function PasswordInput() {
 
   return (
     <div>
-      <label 
+      <label
         htmlFor="password"
-        className='flex self-center mx-8 block mb-2 text-sm font-medium'>Password</label>
-      <div>
+        className="flex self-center mx-8 block mb-2 text-sm font-medium"
+      >
+        Password
+      </label>
+      <div className="flex justify-between items-center shadow-sm bg-gray-50 border mx-8 border-gray-300 text-sm font-medium">
         <input
           value={"password"}
           type={visible ? "text" : "password"}
           id="password"
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
+          className='bg-gray-200 text-gray-900 text-sm w-[300px] border-none focus:border-none
+          '
         ></input>
         <div className="p-2">
           {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
